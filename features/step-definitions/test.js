48d8a371b3f1be7f88fb0b59792c84c4e8b2f3e2
Nandish Siddaiah <AL2297@accionlabs.in>
2018-06-24 16:21:47 +0530

    Modified feature

--- features/step-definitions/test.js
+++ features/step-definitions/test.js
@@ -10,7 +10,7 @@ defineSupportCode(function({Given, When, Then}) {
       browser.pause(5000);
     });
 
-    When(/^I search for new result \"([^\"]*)\"$/, (text) => {
+    When(/^I search for another new result \"([^\"]*)\"$/, (text) => {
       browser.setValue('#lst-ib', text);
       browser.pause(5000);
       expect(1).to.equal(2)
