8abdd27c9c0b2dd51655fbd0ce1a7b474139a05d
Nandish Siddaiah <AL2297@accionlabs.in>
2018-06-24 09:57:05 +0530

    First project commit

---
+++ features/step-definitions/test.js
@@ -0,0 +1,18 @@
+let {defineSupportCode} = require('cucumber');
+
+defineSupportCode(function({Given, When, Then}) {
+    Given(/^I navigate to Google$/, () => {
+      browser.url('http://www.google.com')
+    }); 
+
+    When(/^I search for \"([^\"]*)\"$/, (text) => {
+      browser.setValue('#lst-ib', text);
+      browser.pause(5000);
+    });
+
+    When(/^I search for another new result \"([^\"]*)\"$/, (text) => {
+      browser.setValue('#lst-ib', text);
+      browser.pause(5000);
+    });
+
+});
---
+++ features/test.feature
@@ -0,0 +1,12 @@
+Feature: Cucumber proof of concept
+
+Background:
+  Given I navigate to Google
+
+Scenario: First Scenario
+  
+  When I search for "Formula 1"
+
+Scenario: Second Scenario
+  
+  When I search for another new result "Grand Prix"
